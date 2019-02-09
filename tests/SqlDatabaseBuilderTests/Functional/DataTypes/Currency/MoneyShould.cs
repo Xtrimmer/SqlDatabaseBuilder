04a2ae2c580b878dc831b459ca3ac1b26a365118
Jeff Trimmer <trimmerjeff@gmail.com>
2019-02-09 15:41:41 -0800

    Add Currency tests

---
+++ tests/SqlDatabaseBuilderTests/Functional/DataTypes/Currency/MoneyShould.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xtrimmer.SqlDatabaseBuilder;
+using Xunit;
+
+namespace Xtrimmer.SqlDatabaseBuilderTests.Functional
+{
+    public class MoneyShould
+    {
+        [Fact]
+        public void ReturnCorrectDefinition()
+        {
+            DataType dataType = DataType.Money();
+            Assert.Equal("money", dataType.Definition);
+        }
+
+        [Fact]
+        public void ReturnCorrectSize()
+        {
+            DataType dataType = DataType.Money();
+            Assert.Equal(8, dataType.Size);
+        }
+    }
+}
---
+++ tests/SqlDatabaseBuilderTests/Functional/DataTypes/Currency/SmallMoneyShould.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xtrimmer.SqlDatabaseBuilder;
+using Xunit;
+
+namespace Xtrimmer.SqlDatabaseBuilderTests.Functional
+{
+    public class SmallMoneyShould
+    {
+        [Fact]
+        public void ReturnCorrectDefinition()
+        {
+            DataType dataType = DataType.SmallMoney();
+            Assert.Equal("smallmoney", dataType.Definition);
+        }
+
+        [Fact]
+        public void ReturnCorrectSize()
+        {
+            DataType dataType = DataType.SmallMoney();
+            Assert.Equal(4, dataType.Size);
+        }
+    }
+}
