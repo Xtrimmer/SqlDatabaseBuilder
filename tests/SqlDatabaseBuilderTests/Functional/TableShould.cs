c721fbe142cc62b546e4952cccd6b00ab5f3c408
Jeff Trimmer <trimmerjeff@gmail.com>
2019-02-10 09:01:14 -0800

    Add table test

---
+++ tests/SqlDatabaseBuilderTests/Functional/TableShould.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Xtrimmer.SqlDatabaseBuilder;
+using Xunit;
+
+namespace Xtrimmer.SqlDatabaseBuilderTests.Functional
+{
+    public class TableShould
+    {
+        [Fact]
+        public void ThrowExceptionWhenCreatingWithNoColumns()
+        {
+            string tableName = nameof(ThrowExceptionWhenCreatingWithNoColumns);
+            Table table = new Table(tableName);
+            using (SqlConnection sqlConnection = new SqlConnection(""))
+            {
+                table.Create(sqlConnection);
+            }
+        }
+    }
+}
