ee70df7283013af9d67d1d97aa0b629c866ffcea
Jeff Trimmer <trimmerjeff@gmail.com>
2019-02-10 08:29:58 -0800

    Extract this nested ternary operation into an independent statement to avoid future confusion.

--- src/SqlDatabaseBuilder/DataTypes/CharacterSet/VariableCharacterSet.cs
+++ src/SqlDatabaseBuilder/DataTypes/CharacterSet/VariableCharacterSet.cs
@@ -5,10 +5,18 @@ using System.Text;
 namespace Xtrimmer.SqlDatabaseBuilder
 {
     abstract class VariableCharacterSet : CharacterSet
-    {        
+    {
         protected const int MAX_SIZE = int.MaxValue;
         protected bool isMax = false;
-        protected override string NSpecification => isMax ? "(max)" : (n == 1 ? "" : $"({n.ToString()})");
+
+        protected override string NSpecification
+        {
+            get
+            {
+                string nDefinition = n == 1 ? "" : $"({n.ToString()})";
+                return isMax ? "(max)" : nDefinition;
+            }
+        }
 
         public int N
         {
