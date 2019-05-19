39aca51bca43ab241673be5e8207618482353e09
Mauricio Beithia <mauriciobeithia@yahoo.com>
2019-05-18 19:27:38 -0500

    Add DEFAULT constraint unit test.

--- tests/SqlDatabaseBuilderTests/Extensions/TestTableHelper.cs
+++ tests/SqlDatabaseBuilderTests/Extensions/TestTableHelper.cs
@@ -22,5 +22,31 @@ namespace Xtrimmer.SqlDatabaseBuilderTests
                 }
             }
         }
+
+        internal static readonly Dictionary<DataType, object> DataTypeValues = new Dictionary<DataType, object>
+        {
+            {DataType.BigInt(), (long)1},
+            {DataType.SmallInt(), (short)2},
+            {DataType.Int(), 3},
+            {DataType.TinyInt(), (byte)4},
+            {DataType.Bit(), 0},
+            {DataType.Money(), (decimal)5.0000},
+            {DataType.SmallMoney(), (decimal)6.0000},
+            {DataType.Decimal(), (decimal)7.0000},
+            {DataType.Numeric(), (decimal)8.0000},
+            {DataType.Float(), 9.0000},
+            {DataType.Real(), (float)10.0000},
+            {DataType.VarChar(7), "'VarChar'"},
+            {DataType.Char(4), "'Char'"},
+            {DataType.VarBinary(1), (byte)255},
+            {DataType.Binary(1), (byte)128},
+            {DataType.NVarChar(8), "'NVarChar'"},
+            {DataType.NChar(5), "'Nchar'"},
+            {DataType.Date(), "'2019-05-12T00:00:00.0000000'"},
+            {DataType.DateTime(), "'2019-05-12T12:30:20'"},
+            {DataType.DateTime2(), "'2018-04-23T08:20:40.5'"},
+            {DataType.Time(), "'05:40:00'"},
+            {DataType.UniqueIdentifier(), "'00000000-0000-0000-0000-000000000000'"},
+        };
     }
 }
--- tests/SqlDatabaseBuilderTests/Manual/ColumnShould.cs
+++ tests/SqlDatabaseBuilderTests/Manual/ColumnShould.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Data.SqlClient;
 using Xtrimmer.SqlDatabaseBuilder;
 using Xunit;
+using System.Linq;
 
 namespace Xtrimmer.SqlDatabaseBuilderTests.Manual
 {
@@ -25,6 +26,126 @@ namespace Xtrimmer.SqlDatabaseBuilderTests.Manual
                 Assert.True(AllDataTypesMatch(table, sqlConnection));
                 table.Drop(sqlConnection);
             }
+        }
+
+        [Fact]
+        public void AddDefaultConstraintWithoutName()
+        {
+            string tableName = nameof(AddDefaultConstraintWithoutName);
+            Table table = new Table(tableName);
+            Column[] columns = TestTableHelper.DataTypeValues.Keys.Select(k => new Column(k.Definition, k)).ToArray();
+            table.Columns.AddAll(columns);
+            //for each column on the table, add a default type
+            table.Columns.ForEach(c => c.Default = new Default(TestTableHelper.DataTypeValues[c.DataType]));
+            //create table in db
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                Assert.False(table.IsTablePresentInDatabase(sqlConnection));
+                table.Create(sqlConnection);
+                Assert.True(table.IsTablePresentInDatabase(sqlConnection));
+                //insert rows without any values
+                using (SqlCommand sqlCommand = sqlConnection.CreateCommand())
+                {
+                    string sql = $"INSERT {tableName} DEFAULT VALUES";
+                    sqlCommand.CommandText = sql;
+                    int rowsUpdated = sqlCommand.ExecuteNonQuery();
+                    Assert.Equal(1, rowsUpdated);
+                }
+
+                //for each column we need to look up the value that was entered vs the expected value
+                using (SqlCommand sqlCommand = sqlConnection.CreateCommand())
+                {
+                    string sql = $"SELECT * FROM {tableName}";
+                    sqlCommand.CommandText = sql;
+                    SqlDataReader dataReader = sqlCommand.ExecuteReader();
+                    dataReader.Read();
+                    object[] expectedValues = TestTableHelper.DataTypeValues.Values.ToArray();
+                    for (var i = 0; i < dataReader.FieldCount; i++)
+                    {
+                        VerifyDefaultValueIsWhatWasDefined(dataReader, expectedValues, i);
+                    }
+                    dataReader.Close();
+                }
+                table.Drop(sqlConnection);
+            }
+
+        }
+
+        private static void VerifyDefaultValueIsWhatWasDefined(SqlDataReader dataReader, object[] expectedValues, int i)
+        {
+            object result = dataReader[i];
+            if (result is bool && (bool)result == false) result = 0;
+            object expected = expectedValues[i];
+            if (expected is string) expected = ((string)expected).Replace("'", "");
+            if (result is byte[]) expected = new byte[] { (byte)expected };
+            if (result is DateTime) expected = Convert.ToDateTime(expected);
+            if (result is TimeSpan) expected = TimeSpan.Parse((string)expected);
+            if (result is Guid) expected = Guid.Parse((string)expected);
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void AddDefaultConstraintWithName()
+        {
+            string expectedDefaultName = "testDefault";
+            object actualDefaultName = TestDefaultConstraint(expectedDefaultName, "'VarChar'", nameof(AddDefaultConstraintWithName));
+            Assert.Equal(expectedDefaultName, actualDefaultName);
+        }
+
+        [Fact]
+        public void AddDefaultConstraintWithNullNameAndNullValue()
+        {
+            string expectedDefaultName = null;
+            object actualDefaultName = TestDefaultConstraint(expectedDefaultName, null, nameof(AddDefaultConstraintWithNullNameAndNullValue));
+            Assert.Equal(expectedDefaultName, actualDefaultName);
+
+        }
+
+        [Fact]
+        public void AddDefaultConstraintWithNameAndNullValue()
+        {
+            string expectedDefaultName = "testDefault";
+            object actualDefaultName = TestDefaultConstraint(expectedDefaultName, null, nameof(AddDefaultConstraintWithNameAndNullValue));
+            Assert.Null(actualDefaultName);
+        }
+
+        private object TestDefaultConstraint(string defaultName, string defaultValue, string tableName)
+        {
+            string columnName = DataType.VarChar().Definition;
+            object actualDefaultName;
+            int length = defaultValue == null ? 1 : defaultValue.Length;
+            Table table = new Table(tableName);
+            Column column = new Column(columnName, DataType.VarChar(length));
+            column.Default = new Default(defaultName, defaultValue);
+            table.Columns.Add(column);
+
+            using (SqlConnection sqlConnection = new SqlConnection(connectionString))
+            {
+                sqlConnection.Open();
+                Assert.False(table.IsTablePresentInDatabase(sqlConnection), "Table already exists in database");
+                table.Create(sqlConnection);
+                Assert.True(table.IsTablePresentInDatabase(sqlConnection));
+                using (SqlCommand sqlCommand = sqlConnection.CreateCommand())
+                {
+                    string sql = $@"
+                        SELECT 
+                            constraints.name AS DefaultConstraintName
+                        FROM 
+                            sys.default_constraints constraints
+                            INNER JOIN sys.columns columns ON (constraints.parent_object_id = columns.object_id AND constraints.parent_column_id = columns.column_id)
+                            INNER JOIN sys.tables tables ON (tables.object_id = columns.object_id)
+                        WHERE 
+                            tables.name = '{tableName}'
+                            AND columns.name = '{columnName}';
+                            ";
+                    sqlCommand.CommandText = sql;
+                    actualDefaultName = sqlCommand.ExecuteScalar();
+
+                }
+                table.Drop(sqlConnection);
+            }
+            return actualDefaultName;
         }
 
         [Fact]
