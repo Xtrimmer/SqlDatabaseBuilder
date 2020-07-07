cbda374bf9ccb7bd9bedd56fd5b5f246a42799af
Jeff Trimmer <trimmerjeff@gmail.com>
2020-07-06 21:13:08 -0700

    Remove unused imports.

--- tests/SqlDatabaseBuilderTests/Functional/DataTypes/Temporal/DateTimeShould.cs
+++ tests/SqlDatabaseBuilderTests/Functional/DataTypes/Temporal/DateTimeShould.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xtrimmer.SqlDatabaseBuilder;
+﻿using Xtrimmer.SqlDatabaseBuilder;
 using Xunit;
 
 namespace Xtrimmer.SqlDatabaseBuilderTests.Functional
